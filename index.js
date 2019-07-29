57800d1c95195322ce754254ccb54f6bf3b21c0a
Donley P'Simer <donleyp@ip-172-26-5-115.us-west-2.compute.internal>
2019-07-29 06:34:50 +0000

    changed App to instantiated class and moved it into lib

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-/**
- * The main entry point for the server-side of the service.
- * Responsibilities include 
- *  - basic runtime environment setup.
- *  - reading/managing configuration.
- *  - gluing major components together.
- */
-const Server = require('./lib/server');
-const CheckWorker = require('./lib/check_worker.js');
-const LogWorker = require('./lib/log_worker');
+const App = require('./lib/app');
 
-class App {
-    static init() {
-        // Create an instance of the server.
-        const server = new Server();
-        server.init();
-
-        // Worker is a static class, just call init():
-        const checkWorker = new CheckWorker();
-        checkWorker.loop().catch(console.log);
-
-        // Log Worker
-        const logWorker = new LogWorker();
-        logWorker.loop().catch(console.log);
-    }
-}
-
-App.init();
-
-module.export = { App };
+const app = new App();
+app.init();
